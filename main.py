057fb88b8ff4b8ef89bbe6892c24e7dd5b3777c5
Rima Danielyan <rimadanielyan643@gmail.com>
2026-05-25 17:30:45 +0300

    Добавлены файлы для деплоя на Render

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from app.database.database import Base, engine
-from app.api import motors, analysis, pages, reports  # добавили reports
+from app.api import motors, analysis, pages, reports
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="DPT Selector",
-    description="Multi-criteria DC motor selection system",
+    description="Многокритериальный выбор ДПТ",
     version="1.0.0"
 )
 
 app.include_router(pages.router)
 app.include_router(motors.router,   prefix="/api/motors",   tags=["Motors"])
 app.include_router(analysis.router, prefix="/api/analysis", tags=["Analysis"])
-app.include_router(reports.router,  prefix="/api", tags=["Reports"])
+app.include_router(reports.router,  prefix="/api",          tags=["Reports"])
